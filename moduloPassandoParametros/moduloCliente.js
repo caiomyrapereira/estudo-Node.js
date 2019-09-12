f76476920131a427ac17199bd650203eb2c6f876
caio myra pereira <caiomyrapereira@hotmail.com>
2019-09-12 14:59:53 -0300

    add Passando parâmetros entre  módulos

---
+++ moduloPassandoParametros/moduloCliente.js
@@ -0,0 +1,5 @@
+const saudacoesA = require('./moduloParametro');
+const saudacoesB = require('./moduloParametro')('gabriel', 'matheus', 'daniel');
+
+console.log(saudacoesA('caio', 'leticia', 'alzira'))
+console.log(saudacoesB)
