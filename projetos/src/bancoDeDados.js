06f1ca3b3116f27f5b66c9deb9f1cfe99d2441c8
caio myra pereira <caiomyrapereira@hotmail.com>
2019-09-26 16:22:01 -0300

    add get ,post ,put e delete

---
+++ projetos/src/bancoDeDados.js
@@ -0,0 +1,38 @@
+const sequence = {
+    _id: 1,
+    id: function() {
+        return this._id++;
+    }
+}
+
+const produtos = {}
+
+function salvarProduto(produto) {
+    if (!produto.id)
+        produto.id = sequence.id();
+    produtos[produto.id] = produto;
+    return produto;
+}
+
+function getProduto(id) {
+    if (!!produtos[id])
+        return produtos;
+    return {};
+}
+
+function getProdutos() {
+    return Object.values(produtos);
+}
+
+function escluirProduto(id) {
+    const produto = produtos[id]
+    delete produtos[id];
+    return produto;
+}
+
+module.exports = {
+    salvarProduto,
+    getProduto,
+    getProdutos,
+    escluirProduto
+};
--- projetos/src/servidor.js
+++ projetos/src/servidor.js
@@ -1,16 +1,43 @@
 const port = 3003;
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const bancoDeDados = require('./bancoDeDados');
+console.log(bodyParser)
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', function(req, res, next) {
-    res.send({
-        nome: 'notebook',
-        preco: 2200
-    }); // converter para formato json
+    res.send(bancoDeDados.getProdutos()); // converter para formato json
+})
+
+app.get('/produtos/:id', function(req, res, next) {
+    res.send(bancoDeDados.getProduto(req.params.id));
+})
+
+app.post('/produtos', function(req, res, next) {
+    const produto = bancoDeDados.salvarProduto({
+        name: req.body.name,
+        preco: req.body.preco
+    });
+    res.send(produto) //json
+
+})
+app.put('/produtos/:id', function(req, res, next) {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        name: req.body.name,
+        preco: req.body.preco
+    });
+    res.send(produto) //json
+
+})
+app.delete('/produtos/:id', function(req, res, next) {
+    const produto = bancoDeDados.escluirProduto(req.params.id);
+    res.send(produto) //json
+
 })
 
 app.listen(port, function() {
     console.log('my port:' + port)
 })
-
-console.log(express())
